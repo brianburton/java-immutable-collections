0f9fb919860bf21c5f1ccee9de2de8d22fc73280
Brian Burton <brian@burton-computer.com>
2014-06-20 23:05:15 -0400

    simplifies TrieArray.Builder implementation
    Builds array up from leaves.  Now safe to call multiple times per builder.

--- src/main/java/org/javimmutable/collections/array/trie32/TrieArray.java
+++ src/main/java/org/javimmutable/collections/array/trie32/TrieArray.java
@@ -46,8 +46,10 @@ import org.javimmutable.collections.common.AbstractJImmutableArray;
 import org.javimmutable.collections.common.MutableDelta;
 
 import javax.annotation.concurrent.Immutable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 @Immutable
 public class TrieArray<T>
@@ -195,26 +197,60 @@ public class TrieArray<T>
     public static class Builder<T>
             implements MutableBuilder<T, TrieArray<T>>
     {
-        private BuilderImpl<T> builderImpl = new LeafBuilderImpl<T>();
-        private boolean built;
+        private List<TrieNode<T>> leaves = new ArrayList<TrieNode<T>>();
 
         @Override
         public Builder<T> add(T value)
         {
-            builderImpl = builderImpl.add(value);
+            final TrieNode<T> leaf = LeafTrieNode.of(leaves.size(), value);
+            leaves.add(leaf);
             return this;
         }
 
         @Override
         public TrieArray<T> build()
         {
-            if (built) {
-                throw new IllegalStateException();
+            int nodeCount = leaves.size();
+            if (nodeCount == 0) {
+                return TrieArray.of();
             }
-            built = true;
 
-            builderImpl = builderImpl.finish();
-            return builderImpl.build();
+            if (nodeCount == 1) {
+                return new TrieArray<T>(leaves.get(0), 1);
+            }
+
+            List<TrieNode<T>> dst = new ArrayList<TrieNode<T>>();
+            List<TrieNode<T>> src = leaves;
+            int shift = 0;
+            while (nodeCount > 1) {
+                int dstOffset = 0;
+                int srcOffset = 0;
+                while (srcOffset < nodeCount) {
+                    final int count = Math.min(32, nodeCount - srcOffset);
+                    TrieNode<T>[] nodes = allocate(count);
+                    for (int i = 0; i < count; ++i) {
+                        nodes[i] = src.get(srcOffset++);
+                    }
+                    TrieNode<T> branch;
+                    switch (count) {
+                    case 1:
+                        branch = SingleBranchTrieNode.forBranchIndex(shift, 0, nodes[0]);
+                        break;
+                    case 32:
+                        branch = new FullBranchTrieNode<T>(shift, nodes);
+                        break;
+                    default:
+                        branch = MultiBranchTrieNode.forEntries(shift, nodes);
+                        break;
+                    }
+                    set(dst, dstOffset++, branch);
+                }
+                shift += 5;
+                src = dst;
+                nodeCount = dstOffset;
+            }
+            assert nodeCount == 1;
+            return new TrieArray<T>(dst.get(0), leaves.size());
         }
 
         @Override
@@ -267,168 +303,23 @@ public class TrieArray<T>
             }
             return this;
         }
-    }
 
-    private static abstract class BuilderImpl<T>
-    {
-        protected BranchBuilderImpl<T> parentBuilder;
-        protected TrieNode<T>[] nodes;
-        protected int nodeCount;
-        final int shift;
-
-        protected BuilderImpl(int shift)
+        private void set(List<TrieNode<T>> dst,
+                         int index,
+                         TrieNode<T> node)
         {
-            this.nodes = allocate(32);
-            this.nodeCount = 0;
-            this.shift = shift;
-        }
-
-        abstract BuilderImpl<T> add(T value);
-
-        abstract TrieArray<T> build();
-
-        abstract int size();
-
-        abstract BuilderImpl<T> finish();
-
-        protected BuilderImpl<T> getRoot()
-        {
-            return parentBuilder != null ? parentBuilder.getRoot() : this;
-        }
-
-        protected TrieNode<T> createBranch(int shift,
-                                           TrieNode<T>[] nodes,
-                                           int nodeCount)
-        {
-            switch (nodeCount) {
-            case 1:
-                return SingleBranchTrieNode.forBranchIndex(shift, 0, nodes[0]);
-
-            case 32:
-                return new FullBranchTrieNode<T>(shift, nodes.clone());
-
-            default:
-                TrieNode<T>[] branchArray = allocate(nodeCount);
-                System.arraycopy(nodes, 0, branchArray, 0, nodeCount);
-                return MultiBranchTrieNode.forEntries(shift, branchArray);
-            }
-        }
-
-        protected void addNode(TrieNode<T> node)
-        {
-            nodes[nodeCount] = node;
-            nodeCount += 1;
-            if (nodeCount == 32) {
-                if (parentBuilder == null) {
-                    parentBuilder = new BranchBuilderImpl<T>(shift + 5, this);
-                }
-                parentBuilder.acceptChildNode(nodes, nodeCount);
-                nodeCount = 0;
+            if (index < dst.size()) {
+                dst.set(index, node);
+            } else {
+                assert index == dst.size();
+                dst.add(node);
             }
         }
 
         @SuppressWarnings("unchecked")
-        private static <T> TrieNode<T>[] allocate(int size)
+        private TrieNode<T>[] allocate(int size)
         {
             return new TrieNode[size];
-        }
-    }
-
-    private static class BranchBuilderImpl<T>
-            extends BuilderImpl<T>
-    {
-        private final BuilderImpl<T> childBuilder;
-
-        private BranchBuilderImpl(int shift,
-                                  BuilderImpl<T> childBuilder)
-        {
-            super(shift);
-            this.childBuilder = childBuilder;
-        }
-
-        @Override
-        BuilderImpl<T> add(T value)
-        {
-            return childBuilder.add(value);
-        }
-
-        @Override
-        TrieArray<T> build()
-        {
-            assert parentBuilder == null;
-            assert nodeCount > 0;
-            return new TrieArray<T>(createBranch(shift, nodes, nodeCount).trimmedToMinimumDepth(), size());
-        }
-
-        void acceptChildNode(TrieNode<T>[] childNodes,
-                             int childNodeCount)
-        {
-            addNode(createBranch(shift - 5, childNodes, childNodeCount));
-        }
-
-        @Override
-        int size()
-        {
-            return childBuilder.size();
-        }
-
-        @Override
-        BuilderImpl<T> finish()
-        {
-            childBuilder.finish();
-            if (parentBuilder != null && nodeCount > 0) {
-                parentBuilder.acceptChildNode(nodes, nodeCount);
-            }
-            return getRoot();
-        }
-    }
-
-    private static class LeafBuilderImpl<T>
-            extends BuilderImpl<T>
-    {
-        private int size;
-
-        private LeafBuilderImpl()
-        {
-            super(0);
-        }
-
-        @Override
-        BuilderImpl<T> add(T value)
-        {
-            addNode(LeafTrieNode.of(size, value));
-            size += 1;
-            return getRoot();
-        }
-
-        @Override
-        TrieArray<T> build()
-        {
-            assert parentBuilder == null;
-            assert nodeCount == size;
-            switch (size) {
-            case 0:
-                return TrieArray.of();
-            case 1:
-                return new TrieArray<T>(nodes[0], 1);
-            default:
-                return new TrieArray<T>(createBranch(0, nodes, nodeCount), size);
-            }
-        }
-
-        @Override
-        int size()
-        {
-            return size;
-        }
-
-        @Override
-        BuilderImpl<T> finish()
-        {
-            if (parentBuilder != null && nodeCount > 0) {
-                parentBuilder.acceptChildNode(nodes, nodeCount);
-            }
-            return getRoot();
         }
     }
 }
