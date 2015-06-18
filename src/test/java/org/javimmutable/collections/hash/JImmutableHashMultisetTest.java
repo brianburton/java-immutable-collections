c1928c7abc39402e67f538da6e4533a778c20780
Angela Burton <angela.j.burton@vanderbilt.edu>
2015-06-18 16:39:25 -0400

    hash multiset unit tests

--- src/test/java/org/javimmutable/collections/hash/JImmutableHashMultisetTest.java
+++ src/test/java/org/javimmutable/collections/hash/JImmutableHashMultisetTest.java
@@ -43,13 +43,185 @@ import org.javimmutable.collections.JImmutableSet;
 import org.javimmutable.collections.common.StandardJImmutableMultisetTests;
 import org.javimmutable.collections.cursors.StandardCursorTest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class JImmutableHashMultisetTest
-    extends TestCase
+        extends TestCase
 {
     public void testStandard()
     {
         StandardJImmutableMultisetTests.verifyMultiset(JImmutableHashMultiset.<Integer>of());
     }
+
+    public void test()
+    {
+        List<String> valuesL = Arrays.asList("tennant", "smith", "capaldi", "eccleston");
+        JImmutableMultiset<String> valuesM = JImmutableHashMultiset.<String>of().union(valuesL);
+        valuesM = valuesM.setCount("tennant", 10).setCount("smith", 11).setCount("capaldi", 12).setCount("eccleston", 9);
+
+        JImmutableMultiset<String> jmet = JImmutableHashMultiset.<String>of();
+        assertTrue(jmet.isEmpty());
+        assertEquals(0, jmet.size());
+        assertEquals(0, jmet.valueCount());
+        assertEquals(false, jmet.contains("tennant"));
+        assertEquals(false, jmet.contains("smith"));
+        assertEquals(false, jmet.contains("capaldi"));
+        assertEquals(false, jmet.contains("eccleston"));
+        assertEquals(false, jmet.containsAny(valuesL));
+        assertEquals(false, jmet.containsAll(valuesL));
+        assertEquals(false, jmet.containsAllOccurrences(valuesL));
+        assertEquals(false, jmet.containsAllOccurrences(valuesM));
+
+        jmet = jmet.insert("TENNANT".toLowerCase(), 10);
+        assertFalse(jmet.isEmpty());
+        assertEquals(1, jmet.size());
+        assertEquals(10, jmet.valueCount());
+        assertEquals(true, jmet.contains("tennant", 10));
+        assertEquals(false, jmet.contains("smith"));
+        assertEquals(false, jmet.contains("capaldi"));
+        assertEquals(false, jmet.contains("eccleston"));
+        assertEquals(true, jmet.containsAny(valuesL));
+        assertEquals(false, jmet.containsAll(valuesL));
+        assertEquals(false, jmet.containsAllOccurrences(valuesL));
+        assertEquals(false, jmet.containsAllOccurrences(valuesM));
+
+        jmet = jmet.insert("SMITH".toLowerCase(), 11);
+        assertFalse(jmet.isEmpty());
+        assertEquals(2, jmet.size());
+        assertEquals(21, jmet.valueCount());
+        assertEquals(true, jmet.contains("tennant", 10));
+        assertEquals(true, jmet.contains("smith", 11));
+        assertEquals(false, jmet.contains("capaldi"));
+        assertEquals(false, jmet.contains("eccleston"));
+        assertEquals(true, jmet.containsAny(valuesL));
+        assertEquals(false, jmet.containsAll(valuesL));
+        assertEquals(false, jmet.containsAllOccurrences(valuesL));
+        assertEquals(false, jmet.containsAllOccurrences(valuesM));
+
+        assertSame(jmet, jmet.union(Arrays.asList("tennant", "smith")));
+        assertSame(jmet, jmet.union(asSet("tennant", "smith")));
+        assertSame(jmet, jmet.union(asJSet("tennant", "smith")));
+        assertSame(jmet, jmet.union(asJMet("tennant", "smith")));
+        assertNotSame(jmet, jmet.union(asJMet("tennant").insert("smith", 12)));
+
+        JImmutableMultiset<String> jmet2 = jmet.union(valuesL);
+        assertFalse(jmet2.isEmpty());
+        assertEquals(4, jmet2.size());
+        assertEquals(23, jmet2.valueCount());
+        assertEquals(true, jmet2.contains("tennant", 10));
+        assertEquals(true, jmet2.contains("smith", 11));
+        assertEquals(true, jmet2.contains("capaldi"));
+        assertEquals(true, jmet2.contains("eccleston"));
+        assertEquals(true, jmet2.containsAny(valuesL));
+        assertEquals(true, jmet2.containsAll(valuesL));
+        assertEquals(true, jmet2.containsAllOccurrences(valuesL));
+        assertEquals(false, jmet2.containsAllOccurrences(valuesM));
+        assertEquals(asSet("tennant", "smith", "capaldi", "eccleston"), jmet2.getSet());
+
+        assertEquals(jmet, jmet.intersection(jmet2));
+        assertEquals(jmet, jmet2.intersection(jmet));
+        assertEquals(jmet, jmet2.deleteOccurrence("capaldi").deleteOccurrence("eccleston"));
+
+        jmet2 = jmet2.union(valuesM);
+        assertFalse(jmet2.isEmpty());
+        assertEquals(4, jmet2.size());
+        assertEquals(42, jmet2.valueCount());
+        assertEquals(true, jmet2.contains("tennant", 10));
+        assertEquals(true, jmet2.contains("smith", 11));
+        assertEquals(true, jmet2.contains("capaldi", 12));
+        assertEquals(true, jmet2.contains("eccleston", 9));
+        assertEquals(true, jmet2.containsAny(valuesL));
+        assertEquals(true, jmet2.containsAll(valuesL));
+        assertEquals(true, jmet2.containsAllOccurrences(valuesL));
+        assertEquals(true, jmet2.containsAllOccurrences(valuesM));
+
+        assertEquals(jmet, jmet2.delete("capaldi").delete("eccleston"));
+        assertEquals(jmet, jmet2.deleteOccurrence("capaldi", 12).deleteOccurrence("eccleston", 9));
+        assertEquals(jmet, jmet2.deleteAll(Arrays.asList("capaldi", "eccleston")));
+
+        List<String> extra = new ArrayList<String>();
+        for (int i = 0; i < 9; ++i) {
+            extra.add("eccleston");
+        }
+        for (int i = 0; i < 12; ++i) {
+            extra.add("capaldi");
+        }
+        assertEquals(jmet, jmet2.deleteAllOccurrences(extra));
+        assertEquals(jmet, jmet2.deleteAllOccurrences(asJMet(extra)));
+        assertEquals(jmet2, jmet.insertAll(extra));
+        assertEquals(jmet2, jmet.insertAll(asJMet(extra)));
+        assertEquals(JImmutableHashMultiset.of(), jmet2.deleteAll());
+
+        JImmutableMultiset<String> jmet3 = asJMet(valuesL).insert("davison").insert("baker");
+        assertFalse(jmet3.isEmpty());
+        assertEquals(6, jmet3.size());
+        assertEquals(6, jmet3.valueCount());
+        assertEquals(true, jmet3.contains("tennant"));
+        assertEquals(true, jmet3.contains("smith"));
+        assertEquals(true, jmet3.contains("capaldi"));
+        assertEquals(true, jmet3.contains("eccleston"));
+        assertEquals(true, jmet3.contains("davison"));
+        assertEquals(true, jmet3.contains("baker"));
+        assertEquals(true, jmet3.containsAny(valuesL));
+        assertEquals(true, jmet3.containsAny(jmet));
+        assertEquals(true, jmet3.containsAny(jmet2));
+        assertEquals(true, jmet3.containsAll(valuesL));
+        assertEquals(true, jmet3.containsAll(jmet));
+        assertEquals(true, jmet3.containsAll(jmet2));
+        assertEquals(true, jmet3.containsAllOccurrences(valuesL));
+        assertEquals(false, jmet3.containsAllOccurrences(jmet));
+        assertEquals(false, jmet3.containsAllOccurrences(jmet2));
+
+        JImmutableMultiset<String> expected = asJMet("tennant", "smith", "capaldi", "eccleston");
+        assertEquals(expected, jmet3.intersection(valuesL));
+        assertEquals(expected, jmet3.intersection(asSet("tennant", "smith", "capaldi", "eccleston")));
+        assertEquals(expected, jmet3.intersection(asJSet("tennant", "smith", "capaldi", "eccleston")));
+        assertEquals(expected, jmet3.intersection(expected));
+
+
+    }
+
+    public void testDeleteAll()
+    {
+        JImmutableMultiset<String> jmet = JImmutableHashMultiset.<String>of().insert("TENNANT").insert("ECCLESTON");
+        assertSame(JImmutableHashMultiset.of(), jmet.deleteAll());
+    }
+
+    private Set<String> asSet(String... args)
+    {
+        Set<String> set = new HashSet<String>();
+        for (String arg : args) {
+            set.add(arg);
+        }
+        return set;
+    }
+
+    private JImmutableSet<String> asJSet(String... args)
+    {
+        JImmutableSet<String> jet = JImmutableHashSet.<String>of();
+        for (String arg : args) {
+            jet = jet.insert(arg);
+        }
+        return jet;
+    }
+
+    private JImmutableMultiset<String> asJMet(String... args)
+    {
+        JImmutableMultiset<String> jmet = JImmutableHashMultiset.<String>of();
+        for (String arg : args) {
+            jmet = jmet.insert(arg);
+        }
+        return jmet;
+    }
+
+    private JImmutableMultiset<String> asJMet(List<String> list)
+    {
+        JImmutableMultiset<String> jmet = JImmutableHashMultiset.<String>of();
+        return jmet.insertAll(list);
+    }
+
 }
