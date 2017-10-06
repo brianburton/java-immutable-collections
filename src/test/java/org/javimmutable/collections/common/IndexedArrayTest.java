3024b41a3c5f6e5d75673561647aaf646c1e4534
Brian Burton <brian@burton-computer.com>
2017-10-05 21:18:45 -0400

    removes broken of() methods

--- src/test/java/org/javimmutable/collections/common/IndexedArrayTest.java
+++ src/test/java/org/javimmutable/collections/common/IndexedArrayTest.java
@@ -10,13 +10,6 @@ public class IndexedArrayTest
 {
     public void test()
     {
-        verifyEquals(IndexedArray.<Integer>of(), list());
-        verifyEquals(IndexedArray.of(1), list(1));
-        verifyEquals(IndexedArray.of(1, 2), list(1, 2));
-        verifyEquals(IndexedArray.of(1, 2, 3), list(1, 2, 3));
-        verifyEquals(IndexedArray.of(1, 2, 3, 4), list(1, 2, 3, 4));
-        verifyEquals(IndexedArray.of(1, 2, 3, 4, 5), list(1, 2, 3, 4, 5));
-        verifyEquals(IndexedArray.of(1, 2, 3, 4, 5, 6), list(1, 2, 3, 4, 5, 6));
         verifyEquals(IndexedArray.retained(new Integer[]{7, 8, 9}), list(7, 8, 9));
         verifyEquals(IndexedArray.copied(new Integer[]{7, 8, 9}), list(7, 8, 9));
     }
