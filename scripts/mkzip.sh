7e0eaf4aaee4aea1ce9cf5b3ea09c39edaecbc99
Brian Burton <brian@burton-computer.com>
2017-10-07 00:11:50 -0400

    updates mkzip.sh to work with release plugin artifacts

--- scripts/mkzip.sh
+++ scripts/mkzip.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-mvn clean package
-cd target || exit 1
+if [ -d target/checkout/target ] ; then
+  cd target/checkout/target
+else
+  mvn clean package
+  cd target || exit 1
+fi
 [ -f javimmutable-collections-*-sources.jar ] || exit 1
 
 base=`basename javimmutable-collections-*-sources.jar -sources.jar`
@@ -12,3 +16,4 @@ mkdir $base
 cp *.jar $base
 cp ../LICENSE.txt $base
 zip -r ${base}.zip $base
+echo created $PWD/${base}.zip
