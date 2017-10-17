de100b4430d19f75f6bef59c5f46e50abf87f0de
Brian Burton <brian@burton-computer.com>
2017-10-16 21:38:02 -0400

    adds split functionality to ArrayCursor

--- src/main/java/org/javimmutable/collections/cursors/ArrayCursor.java
+++ src/main/java/org/javimmutable/collections/cursors/ArrayCursor.java
@@ -1,6 +1,7 @@
 package org.javimmutable.collections.cursors;
 
 import org.javimmutable.collections.Cursor;
+import org.javimmutable.collections.SplitCursor;
 
 import javax.annotation.Nonnull;
 
@@ -9,12 +10,15 @@ public class ArrayCursor<T>
 {
     private final T[] values;
     private final int index;
+    private final int limit;
 
     private ArrayCursor(T[] values,
-                        int index)
+                        int index,
+                        int limit)
     {
         this.values = values;
         this.index = index;
+        this.limit = limit;
     }
 
     @Nonnull
@@ -23,7 +27,19 @@ public class ArrayCursor<T>
         if (source.length == 0) {
             return StandardCursor.of();
         } else {
-            return AbstractStartCursor.cursor(() -> new ArrayCursor<>(source, 0));
+            return AbstractStartCursor.cursor(() -> new ArrayCursor<>(source, 0, source.length));
+        }
+    }
+
+    @Nonnull
+    private static <T> Cursor<T> cursor(@Nonnull T[] source,
+                                        int index,
+                                        int limit)
+    {
+        if (index >= limit) {
+            return StandardCursor.of();
+        } else {
+            return AbstractStartCursor.cursor(() -> new ArrayCursor<>(source, index, limit));
         }
     }
 
@@ -32,12 +48,28 @@ public class ArrayCursor<T>
     public Cursor<T> next()
     {
         final int nextIndex = index + 1;
-        return (nextIndex < values.length) ? new ArrayCursor<T>(values, nextIndex) : super.next();
+        return (nextIndex < limit) ? new ArrayCursor<T>(values, nextIndex, limit) : super.next();
     }
 
     @Override
     public T getValue()
     {
         return values[index];
+    }
+
+    @Override
+    public boolean isSplitAllowed()
+    {
+        return (limit - index) > 1;
+    }
+
+    @Override
+    public SplitCursor<T> splitCursor()
+    {
+        final int splitIndex = index + (limit - index) / 2;
+        if (splitIndex == index) {
+            throw new UnsupportedOperationException();
+        }
+        return new SplitCursor<>(cursor(values, index, splitIndex), cursor(values, splitIndex, limit));
     }
 }
--- src/test/java/org/javimmutable/collections/cursors/ArrayCursorTest.java
+++ src/test/java/org/javimmutable/collections/cursors/ArrayCursorTest.java
@@ -1,16 +1,54 @@
 package org.javimmutable.collections.cursors;
 
 import junit.framework.TestCase;
+import org.javimmutable.collections.Cursor;
+import org.javimmutable.collections.SplitCursor;
 
-import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.*;
+import static org.javimmutable.collections.cursors.StandardCursorTest.listCursorTest;
 
 public class ArrayCursorTest
     extends TestCase
 {
     public void test()
     {
-        StandardCursorTest.emptyCursorTest(ArrayCursor.cursor(new Integer[0]));
-        StandardCursorTest.listCursorTest(Arrays.asList(1), ArrayCursor.cursor(new Integer[]{1}));
-        StandardCursorTest.listCursorTest(Arrays.asList(2, 1), ArrayCursor.cursor(new Integer[]{2, 1}));
+        StandardCursorTest.emptyCursorTest(cursor());
+        listCursorTest(asList(1), cursor(1));
+        listCursorTest(asList(2, 1), cursor(2, 1));
+    }
+
+    public void testSplitAllowed()
+    {
+        assertEquals(false, cursor().start().isSplitAllowed());
+        assertEquals(false, cursor(1).start().isSplitAllowed());
+        assertEquals(true, cursor(1, 2).start().isSplitAllowed());
+        assertEquals(true, cursor(1, 2, 3).start().isSplitAllowed());
+        assertEquals(true, cursor(1, 2, 3, 4).start().isSplitAllowed());
+    }
+
+    public void testSplit()
+    {
+        assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> cursor().start().splitCursor());
+        assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> cursor(1).start().splitCursor());
+        verifySplit(cursor(1, 2).start(), asList(1), asList(2));
+        verifySplit(cursor(1, 2, 3).start(), asList(1), asList(2, 3));
+        verifySplit(cursor(1, 2, 3, 4).start(), asList(1, 2), asList(3, 4));
+    }
+
+    private void verifySplit(Cursor<Integer> cursor,
+                             List<Integer> left,
+                             List<Integer> right)
+    {
+        SplitCursor<Integer> split = cursor.splitCursor();
+        listCursorTest(left, split.getLeft());
+        listCursorTest(right, split.getRight());
+    }
+
+    private Cursor<Integer> cursor(Integer... values)
+    {
+        return ArrayCursor.cursor(values);
     }
 }
