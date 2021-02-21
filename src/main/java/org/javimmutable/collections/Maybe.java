9b5288a83fdd1194251421fdc030b4b615672455
Brian Burton <brian@burton-computer.com>
2021-02-21 10:38:06 -0500

    Improves comments.  Adds copyrights.

--- src/main/java/org/javimmutable/collections/Maybe.java
+++ src/main/java/org/javimmutable/collections/Maybe.java
@@ -1,3 +1,38 @@
+///###////////////////////////////////////////////////////////////////////////
+//
+// Burton Computer Corporation
+// http://www.burton-computer.com
+//
+// Copyright (c) 2021, Burton Computer Corporation
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//     Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//     Neither the name of the Burton Computer Corporation nor the names
+//     of its contributors may be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package org.javimmutable.collections;
 
 import org.javimmutable.collections.common.StreamConstants;
@@ -20,8 +55,8 @@ public abstract class Maybe<T>
     implements IterableStreamable<T>
 {
     /**
-     * Produce a Maybe that is guaranteed not to be empty.  If this Maybe is non-empty
-     * it is returned.  Otherwise the noneMapping function is called to provide a value
+     * Produce a non-empty Maybe.  If this Maybe is non-empty it is returned.  
+     * Otherwise the noneMapping function is called to provide a value
      * for the result Maybe.
      *
      * @param noneMapping produces value if this is empty
@@ -54,8 +89,8 @@ public abstract class Maybe<T>
                                      @Nonnull Func1<? super T, ? extends U> someMapping);
 
     /**
-     * Produce a Maybe that is guaranteed not to be empty.  If this Maybe is non-empty
-     * it is returned.  Otherwise the noneMapping function is called to provide a value
+     * Produce a non-empty Maybe.  If this Maybe is non-empty it is returned.  
+     * Otherwise the noneMapping function is called to provide a value
      * for the result Maybe.
      *
      * @param noneMapping produces value if this is empty
@@ -366,6 +401,8 @@ public abstract class Maybe<T>
     @Nonnull
     public static <T> Maybe<T> some(@Nonnull T value)
     {
+        //noinspection ConstantConditions
+        assert value != null;
         return new Some<>(value);
     }
 
@@ -392,7 +429,7 @@ public abstract class Maybe<T>
     {
         for (T value : collection) {
             if (value != null && predicate.apply(value)) {
-                return maybe(value);
+                return some(value);
             }
         }
         return none();
