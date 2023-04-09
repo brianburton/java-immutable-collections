00d4b867382f17f5bc984fea8d696c66418d1380
Brian Burton <brian@burton-computer.com>
2023-04-09 11:27:18 -0400

    Refactoring Calc.  Adds Result.

---
+++ src/main/java/org/javimmutable/collections/Result.java
@@ -0,0 +1,43 @@
+package org.javimmutable.collections;
+
+import java.util.concurrent.Callable;
+
+public abstract class Result<TSuccess, TFailure> {
+    private Result() {
+    }
+
+
+    public static <TSuccess, TFailure> Result<TSuccess, TFailure> success(TSuccess value) {
+        return new Success<>(value);
+    }
+
+    public static <TSuccess, TFailure> Result<TSuccess, TFailure> failure(TFailure value) {
+        return new Failure<>(value);
+    }
+
+    public static <TSuccess> Result<TSuccess, Exception> attempt(Callable<TSuccess> func) {
+        try {
+            return success(func.call());
+        } catch (Exception error) {
+            return failure(error);
+        }
+    }
+
+    public static class Success<TSuccess, TFailure>
+            extends Result<TSuccess, TFailure> {
+        private final TSuccess value;
+
+        private Success(TSuccess value) {
+            this.value = value;
+        }
+    }
+
+    public static class Failure<TSuccess, TFailure>
+            extends Result<TSuccess, TFailure> {
+        private final TFailure value;
+
+        private Failure(TFailure value) {
+            this.value = value;
+        }
+    }
+}
--- src/test/java/org/javimmutable/collections/ComputeTest.java
+++ src/test/java/org/javimmutable/collections/ComputeTest.java
@@ -43,60 +43,60 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class CalcTest
-    extends TestCase
-{
+public class ComputeTest
+        extends TestCase {
     public void testEager()
-        throws Exception
-    {
+            throws Exception {
         List<Object> values = new ArrayList<>();
-        Calc<Boolean> cb = Calc.eager(10)
-            .apply(values::add)
-            .map(x -> (double)(x + 1))
-            .apply(values::add)
-            .map(x -> x > 10);
+        Compute<Boolean> cb = Compute.success(10)
+                .apply(values::add)
+                .map(x -> (double) (x + 1))
+                .apply(values::add)
+                .map(x -> x > 10);
         assertEquals(Boolean.TRUE, cb.get());
         assertEquals(Arrays.asList(10, 11.0), values);
         assertEquals(Boolean.TRUE, cb.get());
         assertEquals(Arrays.asList(10, 11.0), values);
 
         values.clear();
-        Calc<Boolean> cb2 = Calc.eager(10)
-            .apply(x -> {
-                throw new IOException();
-            })
-            .map(x -> (double)(x + 1))
-            .apply(values::add)
-            .map(x -> x > 10);
+        Compute<Boolean> cb2 = Compute.success(10)
+                .apply(x -> {
+                    throw new IOException();
+                })
+                .map(x -> (double) (x + 1))
+                .apply(values::add)
+                .map(x -> x > 10);
         assertThatThrownBy(cb2::get).isInstanceOf(IOException.class);
         assertEquals(Collections.emptyList(), values);
 
         values.clear();
-        Calc<Boolean> cb3 = Calc.eager(10)
-            .apply(values::add)
-            .map(x -> (double)(x + 1))
-            .apply(x -> {
-                throw new IOException();
-            })
-            .map(x -> x > 10);
+        Compute<Boolean> cb3 = Compute.success(10)
+                .apply(values::add)
+                .map(x -> (double) (x + 1))
+                .apply(x -> {
+                    throw new IOException();
+                })
+                .map(x -> x > 10);
         assertThatThrownBy(cb3::get).isInstanceOf(IOException.class);
         assertEquals(Collections.singletonList(10), values);
 
         values.clear();
-        Calc<String> cb4 = Calc.eager(18)
-            .flatMap(x -> Calc.eager(x + 4))
-            .apply(values::add)
-            .flatMap(x -> Calc.eager(String.valueOf(x)));
+        Compute<String> cb4 = Compute.success(18)
+                .flatMap(x -> Compute.success(x + 4))
+                .apply(values::add)
+                .flatMap(x -> Compute.success(String.valueOf(x)));
         assertEquals("22", cb4.get());
         assertEquals(Collections.singletonList(22), values);
 
         values.clear();
-        Calc<String> cb5 = Calc.eager(10)
-            .flatMap(x -> Calc.eager(1).map(y -> {throw new IOException();}))
-            .apply(values::add)
-            .flatMap(x -> Calc.eager(String.valueOf(x)));
+        Compute<String> cb5 = Compute.success(10)
+                .flatMap(x -> Compute.success(1).map(y -> {
+                    throw new IOException();
+                }))
+                .apply(values::add)
+                .flatMap(x -> Compute.success(String.valueOf(x)));
         assertThatThrownBy(cb5::get).isInstanceOf(IOException.class);
         assertEquals(Collections.emptyList(), values);
     }
@@ -105,57 +105,59 @@ public class CalcTest
         throws Exception
     {
         List<Object> values = new ArrayList<>();
-        Calc<Double> cb0 = Calc.lazy(() -> 10)
-            .apply(values::add)
-            .map(x -> (double)(x + 1))
-            .apply(values::add);
+        Compute<Double> cb0 = Compute.compute(() -> 10)
+                .apply(values::add)
+                .map(x -> (double) (x + 1))
+                .apply(values::add);
         assertEquals(11.0, cb0.get());
         assertEquals(Arrays.asList(10, 11.0), values);
         assertEquals(11.0, cb0.get());
         assertEquals(Arrays.asList(10, 11.0, 10, 11.0), values);
 
         values.clear();
-        Calc<Boolean> cb1 = cb0.map(x -> x > 10);
+        Compute<Boolean> cb1 = cb0.map(x -> x > 10);
         assertEquals(Boolean.TRUE, cb1.get());
         assertEquals(Arrays.asList(10, 11.0), values);
         assertEquals(Boolean.TRUE, cb1.get());
         assertEquals(Arrays.asList(10, 11.0, 10, 11.0), values);
 
         values.clear();
-        Calc<Boolean> cb2 = Calc.lazy(() -> 10)
-            .apply(x -> {
-                throw new IOException();
-            })
-            .map(x -> (double)(x + 1))
-            .apply(values::add)
-            .map(x -> x > 10);
+        Compute<Boolean> cb2 = Compute.compute(() -> 10)
+                .apply(x -> {
+                    throw new IOException();
+                })
+                .map(x -> (double) (x + 1))
+                .apply(values::add)
+                .map(x -> x > 10);
         assertThatThrownBy(cb2::get).isInstanceOf(IOException.class);
         assertEquals(Collections.emptyList(), values);
 
         values.clear();
-        Calc<Boolean> cb3 = Calc.lazy(() -> 10)
-            .apply(values::add)
-            .map(x -> (double)(x + 1))
-            .apply(x -> {
-                throw new IOException();
-            })
-            .map(x -> x > 10);
+        Compute<Boolean> cb3 = Compute.compute(() -> 10)
+                .apply(values::add)
+                .map(x -> (double) (x + 1))
+                .apply(x -> {
+                    throw new IOException();
+                })
+                .map(x -> x > 10);
         assertThatThrownBy(cb3::get).isInstanceOf(IOException.class);
         assertEquals(Collections.singletonList(10), values);
 
         values.clear();
-        Calc<String> cb4 = Calc.lazy(() -> 18)
-            .flatMap(x -> Calc.lazy(() -> x + 4))
-            .apply(values::add)
-            .flatMap(x -> Calc.lazy(() -> String.valueOf(x)));
+        Compute<String> cb4 = Compute.compute(() -> 18)
+                .flatMap(x -> Compute.compute(() -> x + 4))
+                .apply(values::add)
+                .flatMap(x -> Compute.compute(() -> String.valueOf(x)));
         assertEquals("22", cb4.get());
         assertEquals(Collections.singletonList(22), values);
 
         values.clear();
-        Calc<String> cb5 = Calc.lazy(() -> 10)
-            .flatMap(x -> Calc.lazy(() -> {throw new IOException();}))
-            .apply(values::add)
-            .flatMap(x -> Calc.lazy(() -> String.valueOf(x)));
+        Compute<String> cb5 = Compute.compute(() -> 10)
+                .flatMap(x -> Compute.compute(() -> {
+                    throw new IOException();
+                }))
+                .apply(values::add)
+                .flatMap(x -> Compute.compute(() -> String.valueOf(x)));
         assertThatThrownBy(cb5::get).isInstanceOf(IOException.class);
         assertEquals(Collections.emptyList(), values);
     }
