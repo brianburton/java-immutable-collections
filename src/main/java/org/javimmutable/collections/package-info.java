dc98c452957f1dbf800c4d1a3e1513c6858a63ef
Brian Burton <brian@burton-computer.com>
2024-02-28 08:08:07 -0500

    Adds a package info class for main package.

---
+++ src/main/java/org/javimmutable/collections/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * This package contains all of the classes and interfaces intended for public use of the API.
+ * The project wiki provides extensive documentation on the public classes and interfaces.
+ * <p>
+ * Several factory classes provide static methods to create collections:
+ *
+ * <ul>
+ *     <li>{@link org.javimmutable.collections.ILists}</li>
+ *     <li>{@link org.javimmutable.collections.IMaps}</li>
+ *     <li>{@link org.javimmutable.collections.ISets}</li>
+ *     <li>{@link org.javimmutable.collections.IDeques}</li>
+ *     <li>{@link org.javimmutable.collections.IArrays}</li>
+ *     <li>{@link org.javimmutable.collections.IMultisets}</li>
+ *     <li>{@link org.javimmutable.collections.IListMaps}</li>
+ *     <li>{@link org.javimmutable.collections.ISetMaps}</li>
+ * </ul>
+ *
+ * @see <a href="https://github.com/brianburton/java-immutable-collections/wiki">Project Wiki</a>
+ */
+package org.javimmutable.collections;
